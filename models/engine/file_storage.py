14ad83201f04f3d95200250f0b8df75f0b627671
Cristiand187 <cristiand187@hotmail.com>
2020-09-02 21:05:11 -0500

    Web framework - Task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,7 +47,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -61,3 +61,9 @@ class FileStorage:
         if key in self.__objects:
             del self.__objects[key]
             self.save()
+
+    def close(self):
+        """
+        method for deserializing the JSON file to objects
+        """
+        self.reload()
